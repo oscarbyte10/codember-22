b3c181af088d79aade5ced1cd2bc962cf93fa189
oscarbyte10 <oscarblanq@gmail.com>
2022-11-12 12:44:24 +0100

    add main.ts

--- challenge01/index.ts
+++ challenge01/index.ts
@@ -3,18 +3,20 @@ const mapData = (value: string): string[] => value.split("\n\n");
 const REQUIRED_FIELDS = ["usr:", "eme:", "psw:", "loc:", "age:", "fll:"];
 
 const readFile = (): Promise<string> =>
-  Deno.readTextFile("challenge01/users.txt");
+  fetch("https://codember.dev/userrs.txt").then(
+    (response) => response && response.text()
+  ) ?? Deno.readTextFile("challenge01/users.txt");
 
 const isValid = (value: string): boolean =>
   REQUIRED_FIELDS.every((key) => value.includes(key));
 
-readFile().then((data) => {
+export const answer = readFile().then((data) => {
   const dataMapped = mapData(data);
   const validUsers = dataMapped.filter(isValid);
   const size = validUsers.length;
   const lastEntry = validUsers[size - 1];
   const lastUser = lastEntry.split(" ").map((value) => value.split(":")[1])[0];
 
-  const answer = lastUser;
-  console.log(size + answer);
+  const answer = size + lastUser;
+  return answer;
 });
---
+++ main.ts
@@ -0,0 +1,3 @@
+import { answer as answer1 } from "./challenge01/index.ts";
+
+answer1.then((value) => console.log("Challenge 01:", value));
